7a81aebdbb4cc251a0a61608898e625538170e6d
Ilya <iosdsbb@gmail.com>
2025-10-27 20:05:50 +0300

    feat: updated App

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
 import {useState} from "react";
 import styles from '~/app.module.css';
+import {generateId} from "~utils/id.js";
 
 function App() {
   const [value, setValue] = useState('');
   const [list, setList] = useState([]);
   const [error, setError] = useState('');
+  const [isValueValid, setIsValueValid] = useState(false);
 
   const onInputButtonClick = () => {
     const promptValue = prompt('Введите значение');
-    const isValid = validate(promptValue);
-    if (!isValid) {
+    setIsValueValid((isValid) => validate(promptValue));
+    if (!isValueValid) {
       setError('Строка должна быть более 3 символов');
       return;
     }
     setValue(promptValue);
+    setError('');
+  };
+
+  const onAddButtonClick = () => {
+    const id = generateId();
+    const updatedList = [...list, {id, value}];
+    console.log({id, value})
+    setList(updatedList);
+    setValue('');
     setError('');
   };
 
@@ -28,16 +39,21 @@ function App() {
           <output className={styles['currentValue']}>{value}</output>
           "
         </p>
-        <div className={styles['error']}>{error}</div>
+        {error !== "" && <div className={styles['error']}>{error}</div>}
         <div className={styles['buttonsContainer']}>
           <button className={styles['button']} onClick={onInputButtonClick}>Ввести новое</button>
-          <button className={styles['button']} disabled>Добавить в список</button>
+          <button className={styles['button']} onClick={onAddButtonClick} disabled={!isValueValid}>
+            Добавить в список
+          </button>
         </div>
         <div className={styles['listContainer']}>
           <h2 className={styles['listHeading']}>Список:</h2>
-          <p className={styles['noMarginText']}>Нет добавленных элементов</p>
+          {list.length === 0 && <p className={styles['noMarginText']}>Нет добавленных элементов</p>}
           <ul className={styles['list']}>
-            <li className={styles['listItem']}>Первый элемент</li>
+            {/*<li className={styles['listItem']}>Первый элемент</li>*/}
+            {list.map((id, value) =>
+              <li className={styles['listItem']} key={id}>{value}</li>
+            )}
           </ul>
         </div>
       </div>
