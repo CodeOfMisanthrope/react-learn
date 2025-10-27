725452d3160736af64cee4cb35906a595b99a05a
Ilya <iosdsbb@gmail.com>
2025-10-27 20:46:38 +0300

    feat: added utils date

---
+++ src/utils/date.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * @param {Date} date
+ */
+export function convertData(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+
+  return `${day}:${month}:${year} ${hour}:${minute}:${second}`;
+}
